76a1290906c1369591cadf67ecd2ab311963f31e
Silvan Schmidt <silvan.genius@gmail.com>
2023-01-15 17:13:04 +0100

    Backup

--- silksong_announcer/__main__.py
+++ silksong_announcer/__main__.py
@@ -1,7 +1,17 @@
-def main() -> int:
+#!/bin/python3
+
+from steam import Steam
+from decouple import config
+from json import loads
+
+
+def main(STEAM_API_KEY: str, SILKSONG_APP_ID: int) -> int:
+    steam = Steam(STEAM_API_KEY)
+    release_date = loads(str(steam.apps.get_app_details(SILKSONG_APP_ID)))[str(SILKSONG_APP_ID)]["data"]["release_date"]["date"]
+    print(release_date)
+
     return 0
 
 
 if __name__ == "__main__":
-    __import__("sys").exit(main())
-
+    __import__("sys").exit(main(config("STEAM_API_KEY"), 1030300)) # type: ignore
